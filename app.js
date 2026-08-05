e783076638f25633b26b7492221ee38719deef77
shiraj <shirajmujawar03@gmail.com>
2026-08-05 10:37:45 +0530

    Update welcome message to include backend context for Dture

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json({ limit: "10mb" })); // ✅ parse JSON
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World! this is the backend of Dture');
 });
 
 
